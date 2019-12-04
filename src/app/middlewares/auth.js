128d18caee095e4cec4d82be693204f4c664df9b
Talita Azevedo <talita.azevedo360@gmail.com>
2019-12-04 16:00:34 -0300

    Importe de files e avatares

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -13,12 +13,13 @@ export default async (req,res,next) =>{
 
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
     return next();
 
   } catch (err) {
     return res.status(401).json({ error: 'token invalid ' });
   }
-  return next();
+  
 
 
 
