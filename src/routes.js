a1ff87a6d1812489b89b047adbd18c4af30cf45c
Talita Azevedo <talita.azevedo360@gmail.com>
2019-12-04 12:56:31 -0300

    Update de usuário

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,15 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 
+import authMiddleware from './app/middlewares/auth';
+
+
 routes.post('/users',UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware)
+
+routes.put('/users',UserController.update);
 
 export default routes;
 
