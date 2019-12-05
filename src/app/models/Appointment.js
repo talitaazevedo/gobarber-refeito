47f841f8eddf196b1aba123b18164c1e5fd602ee
Talita Azevedo <talita.azevedo360@gmail.com>
2019-12-05 13:24:20 -0300

    Creating Appointments

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,5 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 
+
 class Appointment extends Model {
   static init(sequelize) {
     super.init(
@@ -19,7 +20,7 @@ class Appointment extends Model {
   }
   static associate(models){
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user'});
-    this.belongsTo(models.user,{foreignKey:'provider_id', as: 'provider'});
+    this.belongsTo(models.User,{foreignKey:'provider_id', as: 'provider'});
 
 
   }
